5f1904a2a42c1dd1cd214452415f63f6b824b55b
Durgesh Tiwari <61361185+blackhat955@users.noreply.github.com>
2021-03-21 21:45:14 +0530

    Add files via upload

---
+++ src/optionVal.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "./input.css";
+import Select from "./option.js";
+const selectInput = (props) => {
+  return (
+    <div>
+      <select>
+        <Select />
+      </select>
+    </div>
+  );
+};
+export default selectInput;
